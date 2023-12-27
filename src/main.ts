35116fba2045ac3d3ed0bdbdf1aa7f90d7809dbb
Serhii Rudenko <svr.phoenix@gmail.com>
2023-12-27 20:27:14 +0200

    Added swagger docs

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     snapshot: true,
   });
   const port = app.get(ConfigService).get('PORT');
+
+  const config = new DocumentBuilder()
+    .setTitle('Todos backend application')
+    .setDescription('The end-points API description')
+    .setVersion('1.0')
+    .addTag('todos')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
--- src/modules/todos/dto/todo.dto.ts
+++ src/modules/todos/dto/todo.dto.ts
@@ -1,11 +1,14 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { IsBoolean, IsNotEmpty, IsOptional } from 'class-validator';
 
 export class CreateTodoDto {
   @IsNotEmpty()
+  @ApiProperty()
   title: string;
+
   @IsBoolean()
   @IsOptional()
+  @ApiProperty()
   isCompleted: boolean;
 }
 
